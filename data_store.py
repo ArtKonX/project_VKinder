cf743ae5023d6f6f9c2c3796341b04bf54dde919
Artemij Kondurov <kondurovartemij@yandex.ru>
2023-06-12 19:11:04 +0300

    fixed

--- data_store.py
+++ data_store.py
@@ -2,10 +2,20 @@ import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, Session
 from sqlalchemy import create_engine, MetaData
 from config import db_url_object
+import psycopg2
 
 metadata = MetaData()
 Base = declarative_base()
 
+def create_db(conn):
+    with conn.cursor() as cur:
+        cur.execute("""CREATE TABLE IF NOT EXISTS matches (
+        profile_id INTEGER, 
+        worksheet_id INTEGER,
+        PRIMARY KEY (profile_id, worksheet_id)
+        );
+        """)
+        conn.commit()
 
 
 class Viewed(Base):
@@ -29,6 +39,9 @@ def check_user(engine, profile_id, worksheet_id):
 
 
 if __name__ == '__main__':
+    with psycopg2.connect(database="postgres", user="postgres", password="") as conn:
+        create_db(conn)
+    conn.close()
     engine = create_engine(db_url_object)
     Base.metadata.create_all(engine)
     add_user(engine, , )
